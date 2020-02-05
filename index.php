6e09ba11570fb1fbbd37e7fdfa5fc7b94133ddce
Jocco26 <60679660+Jocco26@users.noreply.github.com>
2020-02-06 00:41:24 +0800

    Add files via upload

--- index.php
+++ index.php
@@ -2,18 +2,17 @@
 
 include('config.php');
 
-
 $facebook_output = '';
 
 //this will redirect login helper
 $facebook_helper = $facebook->getRedirectLoginHelper();
 
-
 //checks if the user is logged in using facebook account
 if(isset($_GET['code']))
 {
-    if(isset($_GET['access_token']))
+    if(isset($_SESSION['access_token']))
     {
+        //if true store session access_token in variable access_token
         $access_token = $_SESSION['access_token'];
     }
     else
@@ -25,10 +24,9 @@ if(isset($_GET['code']))
         $facebook->setDefaultAccessToken($_SESSION['access_token']);
     }
 
-
     //getting facebook user data
     $graph_response = $facebook->get("/me?fields=name,email", $access_token);
-    
+
     //this will return user data
     $facebook_user_info = $graph_response->getGraphUser();
     
@@ -36,8 +34,9 @@ if(isset($_GET['code']))
     if(!empty($facebook_user_info['id']))
     {
         //stores the user's DP
-        $_SESSION['user_image'] = 'http://graph.facebook.com/'.$facebook_user_info['id'].'/picture';
+        $_SESSION['user_image'] = 'http://graph.facebook.com/' .$facebook_user_info['id'] . '/picture';
     }
+
     //checks name
     if(!empty($facebook_user_info['name']))
     {
@@ -53,54 +52,134 @@ if(isset($_GET['code']))
 //if not logged in displays login page
 else
 {
-    $facebook_permissions = ['email'];
-    //stores index page in $facebook_login_url
+    $facebook_permission = ['email'];
 
-    //later to be use to redirect user to index page in order to login
+    $facebook_login_url = $facebook_helper->getLoginUrl('https://donantoniotattoo.herokuapp.com/', $facebook_permissions);
 
-    //by using php echo
-
-    $facebook_login_url = $facebook_helper->getLoginUrl('https://dafblogin.herokuapp.com/', $facebook_permissions);
-
-    //gives index page with clickable link thats leads facebook login url
-    $facebook_login_url = '<div align="center"><a href="'.$facebook_login_url.'"><img src="php-login-with-facebook.gif" /></a></div>';
+    $facebook_login_url = '<p>ur r not logged in</p>';
 }
+
+
+
 ?>
 
-<html>
- <head>
-  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-  <title>PHP Login using Google Account</title>
-  <meta content='width=device-width, initial-scale=1, maximum-scale=1' name='viewport'/>
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
-  <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet" />
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Don Antonio Tattoo</title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+	<script src="https://use.fontawesome.com/releases/v5.0.8/js/all.js"></script>
+	<link href="style02.css" rel="stylesheet">
+</head>
+<body>
+
+<!-- Navigation -->
+
+<nav class="navbar navbar-expand-md navbar-dark bg-dark sticky-top" >
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#"><img src="img/newlogo.png"></a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" 
+        data-target="#navbarResponsive">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href="#">TATTOO</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">PIERCING</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">BLOG</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">SUPPLIES</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">LOCATION</a>
+                </li>
+            </ul>
+    
+        </div>
+            <div>
+            <?php
+
+                if(isset($facebook_login_url))
+                {
+                echo $facebook_login_url;
+                }
+                else{
+                 echo '<p>u r logged in</p>';
+                }
+
+                ?>
+            </div>
+    </div>
+    </nav>
+
+<!--- Image Slider -->
+
+<!--- Fixed background -->
+<section id="features">
+    
+            <div class="DAlogo" id="logo1">
+				<h1 class="DAlogo"><img id="centerlogo" src="img/DA.png"></a></h1>
+				<button class="DAlogo btn btn-outline-light btn-lg" type="button">
+				BOOK NOW
+				</button>
+			</div>
+    
+</section>
+
+<!--- Jumbotron -->
+
+
+<!--- Welcome Section -->
+
+
+<!--- Three Column Section -->
+
+
+<!--- Two Column Section -->
+
+<!--- Emoji Section -->
+
   
- </head>
- <body>
-  <div class="container">
-   <br />
-   <h2 align="center">PHP Login using Google Account(with github)</h2>
-   <br />
-   <div class="panel panel-default">
-    <?php 
-    // checks if the $facebook_login_url set
-    // changes index page content from what is stored in $facebook_login_url
-    if(isset($facebook_login_url))
-    {
-     echo $facebook_login_url;
-    }
-    //displays welcome page with facebook user's credentials using index page
-    else
-    {
-     echo '<div class="panel-heading">Welcome User</div><div class="panel-body">';
-     echo '<img src="'.$_SESSION["user_image"].'" class="img-responsive img-circle img-thumbnail" />';
-     echo '<h3><b>Name :</b> '.$_SESSION['user_name'].'</h3>';
-     echo '<h3><b>Email :</b> '.$_SESSION['user_email_address'].'</h3>';
-     echo '<h3><a href="logout.php">Logout</h3></div>';
-    }
-    ?>
-   </div>
-  </div>
- </body>
+<!--- Meet the team -->
+
+
+<!--- Cards -->
+
+
+<!--- Two Column Section -->
+
+
+<!--- Connect -->
+
+
+<!--- Footer -->
+
+
+</body>
 </html>
+
+
+
+
+
+
+
+
+
+
+
+
+
